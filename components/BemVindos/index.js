97255f9926e42d50806b14896c35946cadb7d409
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-04-02 00:00:59 -0300

    add `BemVindos` component

---
+++ components/BemVindos/index.js
@@ -0,0 +1,40 @@
+function BemVindos() {
+  const itemsData = [
+    {
+      iconSrc: "icones/importacao.png",
+      title: "Importação",
+      text: "Temos anos de experiência com importação e distribuição de frutas frescas de qualidade.",
+    },
+    {
+      iconSrc: "icones/qualidade.png",
+      title: "Qualidade",
+      text: "Nos comprometemos manter boas relações com produtores mundiais e selecionar suas melhores ofertas.",
+    },
+    {
+      iconSrc: "icones/confianca.png",
+      title: "Confiança",
+      text: "Atendimento honesto, transparente e eficiente, com preços altamente competitivos.",
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center py-20 md:py-25 bg-gradient-to-br from-neutral-100 to-neutral-300">
+        <h2 className="text-4xl md:text-5xl font-bold mb-15 md:mb-20 text-center">
+          Bem-Vindo à Mensageira
+        </h2>
+        <div className="flex flex-col md:flex-row text-center px-10">
+          {itemsData.map((item) => (
+            <div className="flex flex-col justify-center items-center px-7 mb-15 md:mb-auto">
+              <img width="95" className="mb-3" src={item.iconSrc} />
+              <h3 className="text-3xl font-semibold mb-4">{item.title}</h3>
+              <p>{item.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BemVindos;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Carousel from "@/components/Carousel";
 import Footer from "@/components/Footer";
+import BemVindos from "@/components/BemVindos";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Carousel />
+      <BemVindos />
       <Footer />
     </>
   );
