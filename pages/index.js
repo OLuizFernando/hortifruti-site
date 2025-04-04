7069e1aaad54d607332ed07992b5a242abb3f4c7
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2025-04-04 22:36:22 +0000

    add `Produtos` to `Home`

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Carousel from "@/components/Carousel";
 import Footer from "@/components/Footer";
 import BemVindos from "@/components/BemVindos";
+import Produtos from "@/components/Produtos";
 
 function Home() {
   return (
@@ -9,6 +10,10 @@ function Home() {
       <Navbar />
       <Carousel />
       <BemVindos />
+      <div className="w-full flex justify-center bg-neutral-300">
+        <div className="border w-50"></div>
+      </div>
+      <Produtos />
       <Footer />
     </>
   );
