faa0bea1eb70afb0121a04db95431d7158233834
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-04-06 21:09:04 -0300

    change `Produtos` component

--- components/Produtos/index.js
+++ components/Produtos/index.js
@@ -1,19 +1,24 @@
 function NossasFrutas() {
   const productsInfo = [
     {
-      imgSrc: "frutas/maca_red.jpg",
+      imgSrc: "frutas/maca_red.png",
       name: "Maçã Red",
       buttonHref: "/produtos/maca_red",
     },
     {
-      imgSrc: "frutas/maca_verde.jpg",
-      name: "Maçã Verde",
-      buttonHref: "/produtos/maca_verde",
+      imgSrc: "frutas/pera_williams.png",
+      name: "Pera Williams",
+      buttonHref: "/produtos/pera_williams",
     },
     {
-      imgSrc: "frutas/pessego.jpg",
-      name: "Pêssego",
-      buttonHref: "/produtos/maca_red",
+      imgSrc: "frutas/maca_fuji.png",
+      name: "Maçã Fuji",
+      buttonHref: "/produtos/maca_fuji",
+    },
+    {
+      imgSrc: "frutas/maca_gala.png",
+      name: "Maçã Gala",
+      buttonHref: "/produtos/maca_gala",
     },
   ];
 
@@ -21,16 +26,16 @@ function NossasFrutas() {
     <>
       <div className="bg-gradient-to-t from-neutral-100 to-neutral-300 flex flex-col justify-center items-center py-20 md:py-25">
         <h2 className="text-4xl md:text-5xl font-bold mb-15 md:mb-20 text-center">
-          Produtos
+          Principais Frutas
         </h2>
-        <div className="flex flex-col md:flex-row mb-20">
+        <div className="flex flex-col md:flex-row mb-20 px-7 md:px-3 lg:px-10 xl:px-20">
           {productsInfo.map((product) => (
-            <div className="flex flex-col items-center justify-center pb-10 mb-10 mx-auto md:mb-auto md:mx-10 bg-white rounded-md shadow-xl">
-              <img src={product.imgSrc} className="mb-5" />
-              <h3 className="text-2xl mb-7">{product.name}</h3>
+            <div className="flex flex-col items-center justify-center pb-10 mb-10 mx-auto md:mb-auto md:mx-3 lg:mx-5 px-5 bg-white rounded-md shadow-xl">
+              <img src={product.imgSrc} className="mb-5 rounded-md" />
+              <h3 className="text-2xl mb-7 text-center">{product.name}</h3>
               <a
                 href={product.buttonHref}
-                className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 rounded-lg px-4 py-2"
+                className=" text-center text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 rounded-lg px-4 py-2"
               >
                 Saiba mais
               </a>
