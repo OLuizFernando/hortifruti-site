a803245e768065de215f32a72fdcecde33ca3a6f
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-04-07 02:37:15 -0300

    add embed google map and a contact section to `Footer`

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,6 +2,49 @@ function Footer() {
   return (
     <>
       <footer className="bg-white">
+        <div className=" flex items-center mx-auto w-full max-w-screen-xl px-4 py-5">
+          <span className="me-auto md:me-10 text-2xl text-green-700 font-bold">
+            ENTRE EM CONTATO CONOSCO!
+          </span>
+          <div className="flex flex-col md:flex-row justify-center items-center">
+            <a
+              target="_blank"
+              href="https://wa.me/5511966433031"
+              className="bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-lg px-4 py-2 mb-3 md:mb-auto me-auto md:me-7"
+            >
+              <img
+                width="25"
+                src="https://img.icons8.com/?id=85088&format=png&color=ffffff"
+              />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.instagram.com/mensageirafrutas/"
+              className="bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-lg px-4 py-2 mb-3 md:mb-auto me-auto md:me-7"
+            >
+              <img
+                width="25"
+                src="https://img.icons8.com/?id=48841&format=png&color=ffffff"
+              />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/mensageira.frutas"
+              className=" bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-lg px-4 py-2"
+            >
+              <img
+                width="25"
+                src="https://img.icons8.com/?id=pZ9xZYiinken&format=png&color=ffffff"
+              />
+            </a>
+          </div>
+        </div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.9388970939353!2d-46.737906800000005!3d-23.5347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef90766236993%3A0x5736bd85f4bd23bc!2sMensageira%20Comercio%20e%20Importa%C3%A7%C3%A3o%20de%20Frutas%20Ltda!5e0!3m2!1spt-BR!2sbr!4v1744001958083!5m2!1spt-BR!2sbr"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="w-full h-40 md:h-60"
+        ></iframe>
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
@@ -70,7 +113,7 @@ function Footer() {
           <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-gray-500 sm:text-center">
-              © 2025 Mensageira Comércio E Importação de Frutas LTDA.
+              © 2025 Mensageira Comércio e Importação de Frutas LTDA.
             </span>
           </div>
         </div>
