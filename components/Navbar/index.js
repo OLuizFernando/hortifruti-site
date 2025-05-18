743a2c0c91656e5e4ce095accd10dafd08386711
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-05-17 21:45:53 -0300

    update to `Marfruti`

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -16,26 +16,28 @@ function Navbar() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/" className="flex items-center">
             <img
-              src="/logos/minimal_logo.png"
-              className="h-10 sm:h-15 me-1 sm:me-3"
-              alt="Logo Mensageira"
+              src="/logos/logo_symbol.png"
+              className="h-10 sm:h-13 me-3"
+              alt="Logo Marfruti"
             />
-            <span className="self-center text-xl sm:text-3xl mt-2 antecf whitespace-nowrap font-bold text-green-700">
-              MENSAGEIRA
-            </span>
+            <img
+              src="/logos/logo_write.png"
+              className="h-7 sm:h-9"
+              alt="Logo Marfruti"
+            />
           </a>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex lg:order-2 space-x-3 lg:space-x-0 rtl:space-x-reverse">
             <a
               target="_blank"
               href="https://wa.me/5511966433031"
-              className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+              className="text-white bg-[#26530E] hover:bg-[#183409] flex items-center justify-center focus:ring-4 focus:outline-none focus:ring-[#26530E]/40 font-medium rounded-lg px-4 py-2 md:text-lg"
             >
               Saiba mais
             </a>
             <button
               data-collapse-toggle="navbar-sticky"
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100/50 focus:outline-none focus:ring-2 focus:ring-gray-500/50 "
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100/50 focus:outline-none focus:ring-2 focus:ring-gray-500/50 "
               aria-controls="navbar-sticky"
               aria-expanded="false"
             >
@@ -58,17 +60,17 @@ function Navbar() {
             </button>
           </div>
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1"
             id="navbar-sticky"
           >
-            <ul className="flex flex-col text-lg p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
+            <ul className="flex flex-col text-lg p-4 lg:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 lg:space-x-8 rtl:space-x-reverse lg:flex-row lg:mt-0 lg:border-0 lg:bg-transparent">
               {navItems.map((item) => (
                 <li key={item.id}>
                   <a
                     href={item.href}
                     className={`
-                      block py-2 px-3 rounded-sm md:p-0
-                      ${pathname === item.href ? "bg-green-700 text-white md:text-green-700 md:bg-transparent" : "text-black hover:text-green-700"}
+                      block py-2 px-3 rounded-sm lg:p-0 text-xl
+                      ${pathname === item.href ? "bg-[#26530E] text-white lg:text-[#26530E] lg:bg-transparent" : "text-black hover:text-[#26530E]"}
                     `}
                     aria-current="page"
                   >
